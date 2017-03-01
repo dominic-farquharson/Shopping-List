6224f04787a71f270724fac74f9fa8b68514ce8c
Dominic Farquharson <farquharsondominic@gmail.com>
2017-03-01 10:37:51 -0500

    Shopping List App

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170223160938) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.integer  "quantity"
